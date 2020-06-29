7a52d9c75c2b0b95c9bbcefc12b0798a3306c3a2
andy-style <64777265+andy-style@users.noreply.github.com>
2020-06-30 04:35:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ document.getElementById("modalClose").addEventListener("click",function(){
   document.getElementById("modal").classList.remove("active");
   document.getElementById("mask").classList.remove("active");
 })
+
+// 以下、改善の余地あり  ↓↓
+document.getElementById("modal_btn1").addEventListener("click",function(){
+  document.getElementById("modal").classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+})
+
+document.getElementById("modal_btn2").addEventListener("click",function(){
+  document.getElementById("modal").classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+})
+
+document.getElementById("modal_btn3").addEventListener("click",function(){
+  document.getElementById("modal").classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+})
+
+document.getElementById("modal_btn4").addEventListener("click",function(){
+  document.getElementById("modal").classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+})
+// ↑↑   ↑↑
